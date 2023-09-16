85d986570e5b3a23208c8d1a7f43074413f626aa
Millzy <millzyyt08@gmail.com>
2023-09-16 20:59:39 +0100

    base pc plugin

---
+++ pc/ImageCoverExpander/Plugin.cs
@@ -0,0 +1,20 @@
+﻿using IPA;
+using IPA.Logging;
+using SiraUtil.Attributes;
+using SiraUtil.Zenject;
+
+namespace ImageCoverExpander
+{
+    [Plugin(RuntimeOptions.DynamicInit)]
+    [NoEnableDisable]
+    [Slog]
+    // ReSharper disable once UnusedType.Global
+    public class Plugin
+    {
+        [Init]
+        public Plugin(Logger logger, Zenjector zenjector)
+        {
+            zenjector.UseLogger(logger);
+        }
+    }
+}
