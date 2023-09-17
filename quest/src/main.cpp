d60777ef5d4a40c6b51890e1256a22dc9443aaf7
Millzy <millzyyt08@gmail.com>
2023-09-17 19:18:04 +0100

    hooks

--- quest/src/main.cpp
+++ quest/src/main.cpp
@@ -1,35 +1,14 @@
 #include "main.hpp"
+#include "hooking.hpp"
+#include "logging.hpp"
 
-static ModInfo modInfo; // Stores the ID and version of our mod, and is sent to the modloader upon startup
+#include "beatsaber-hook/shared/utils/il2cpp-functions.hpp"
 
-// Loads the config from disk using our modInfo, then returns it for use
-// other config tools such as config-utils don't use this config, so it can be removed if those are in use
-Configuration& getConfig() {
-    static Configuration config(modInfo);
-    return config;
+extern "C" [[maybe_unused]] void setup(ModInfo& info) {
+    info = modInfo;
 }
 
-// Returns a logger, useful for printing debug messages
-Logger& getLogger() {
-    static Logger* logger = new Logger(modInfo);
-    return *logger;
-}
-
-// Called at the early stages of game loading
-extern "C" void setup(ModInfo& info) {
-    info.id = MOD_ID;
-    info.version = VERSION;
-    modInfo = info;
-	
-    getConfig().Load();
-    getLogger().info("Completed setup!");
-}
-
-// Called later on in the game loading - a good time to install function hooks
-extern "C" void load() {
+extern "C" [[maybe_unused]] void load() {
     il2cpp_functions::Init();
-
-    getLogger().info("Installing hooks...");
-    // Install our hooks (none defined yet)
-    getLogger().info("Installed all hooks!");
+    Hooks::InstallHooks(getLogger());
 }
